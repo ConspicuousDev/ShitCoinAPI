4474a5071c4e992b003ce09a8fd3b6109ca73103
Lorenzo9961 <Lorenzogobetti@hotmail.com>
2022-01-06 20:37:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,28 +14,43 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/", (req, res) => {
-    res.json({})
-})
-
-app.get("/api/:endpoint", (req, res) => {
-    let data = shitCoin.getData()
-    let response = {success: data.hasOwnProperty(req.params.endpoint)}
-    if(response.success){
-        response[req.params.endpoint] = data[req.params.endpoint]
-    }else{
-        response.error = "Endpoint not found."
+let routes = [
+    {
+        route: "/",
+        request(req, res) {
+            res.json({
+                message: "Welcome to ShitCoin API.",
+                success: true,
+                version: "0.0.1"
+            })
+        }
+    },
+    {
+        route: "/v1/:endpoint",
+        request(req, res) {
+            let data = shitCoin.getData()
+            let response = {success: data.hasOwnProperty(req.params.endpoint)}
+            if(response.success){
+                response[req.params.endpoint] = data[req.params.endpoint]
+            }else{
+                response.error = "Endpoint not found."
+            }
+            res.json(response)
+        }
+    },
+    {
+        route: ""
     }
-    res.json(response)
-})
+]
 
-app.get("/api/token/:token", (req, res) => {
-    res.json({})
-})
+for(let i = 0; i < routes.length; i++){
+    let route = routes[i]
+    app.get(route.route, (req, res) => {route.request(req, res)})
+}
 
 const PORT = process.env.PORT || 8888
 app.listen(PORT, () => {
-    //console.log(`Server listening at http://localhost:${PORT}`)
+    console.log(`Server listening at http://localhost:${PORT}`)
 })
 
 shitCoin.main()
