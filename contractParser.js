8b422e52133619d18578e8e39650279d4a11ac7b
Lorenzo9961 <lorenzogobetti@hotmail.com>
2022-01-07 01:54:47 -0800

    -

--- contractParser.js
+++ contractParser.js
@@ -1,15 +1,29 @@
 function removeComments(contract){
     let lines = contract.split("\r\n")
     let newLines = []
+    let inComment = false
     for(let i = 0; i < lines.length; i++){
         let line = lines[i].trim()
-        line = line.replace(/\/\/.*/, "")
-
-        if(line.length === 0){
-            continue
+        if(inComment){
+            lineCopy = line.replace(/.*\*\//, "")
+            if(line !== lineCopy){
+                inComment = false
+            }
+            line = line.replace(/.*\*\//, "")
         }
-        newLines.push(line)
-
+        if(!inComment){
+            line = line.replace(/\/\/.*/, "")
+            line = line.replace(/\/\*.*\*\//, "")
+            lineCopy = line.replace(/\/\*.*/, "")
+            if(line !== lineCopy){
+                inComment = true
+            }
+            line = line.replace(/\/\*.*/, "")
+            if(line.trim().length === 0){
+                continue
+            }
+            newLines.push(line)
+        }
     }
     return newLines.join("\n")
 }
