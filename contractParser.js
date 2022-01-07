ece41447dad80a7dcd59c5f8d0cc361d2f8ac578
Lorenzo9961 <lorenzogobetti@hotmail.com>
2022-01-07 02:05:34 -0800

    -

--- contractParser.js
+++ contractParser.js
@@ -22,9 +22,7 @@ function removeComments(contract){
             if(line.trim().length === 0){
                 continue
             }
-            line.replace('"', "'")
-            line.replace('\"', "'")
-            line.replace('\\"', "'")
+            line.replace(/\\"/, "'")
             newLines.push(line)
         }
     }
