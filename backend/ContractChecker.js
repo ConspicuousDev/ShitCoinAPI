fc9be52a8c3bc99b6a25c641b7f9f45768bc45c5
Lorenzo9961 <lorenzogobetti@hotmail.com>
2022-01-22 23:17:26 -0800

    -

--- backend/ContractChecker.js
+++ backend/ContractChecker.js
@@ -1,13 +1,12 @@
 let { web3 } = require("./constants");
 const fetch = require('cross-fetch');
+var solc = require('solc');
+
 
 class ContractChecker{
-    constructor(sourcecode, address){
-        let sourceCodeData = "https://api.bscscan.com/api?module=contract&action=getsourcecode&address=" + address + "&apikey=ZF1G2TWRC54PUCD5AAPT1FCYBXIHYE4BVU"
-        let parameters = fetch(sourceCodeData)
+    constructor(token){
+        
     }
 
-    constructor(address, abi){
 
-    }
 }
