f0bb77b32e3df05cb0c5255f60b3fa0905e829b3
OmniscientARK <lorenzopiuga@gmail.com>
2022-03-07 09:40:42 -0300

    Removed ContractChecker class for later refactoring

--- backend/TokenScorer.js
+++ backend/TokenScorer.js
@@ -1,21 +1,18 @@
-const { compile } = require("solc");
-const solc = require("solc");
-const ContractChecker = require("./ContractChecker")
 const { sleep } = require("./utils");
 
 
-class TokenScorer{
-    constructor(tokenManager){
+class TokenScorer {
+    constructor(tokenManager) {
         this.tokenManager = tokenManager
         console.log(" > TokenScorer initialized.")
         this.scrape()
     }
-    
-    async scrape(){
+
+    async scrape() {
         let scoring = []
-        while(true){
-            let tokens = await this.tokenManager.getTokens({score: null, address: {$nin: scoring}})
-            if(tokens.length === 0){
+        while (true) {
+            let tokens = await this.tokenManager.getTokens({ score: null, address: { $nin: scoring } })
+            if (tokens.length === 0) {
                 await sleep(10000)
                 continue
             }
@@ -24,25 +21,18 @@ class TokenScorer{
             for (let i = 0; i < tokens.length; i++) {
                 const token = tokens[i];
                 scoring.push(token.address)
-                solc.loadRemoteVersion(token.contract.compiler, async (err, solcSnapshot) => {
-                    let value = await this.score(token, solcSnapshot)
-                    if(value === null){
-                        return
-                    }
-                    let score = {value: value, time: Date.now()}
-                    await this.tokenManager.updateToken(token.address, {score: score})
-                    console.log(`[TokenScorer] Token '${token.address}' has been assigned a score of ${score.value}.`)
-                    scoring = scoring.filter((value, index, array) => {return value !== token.address})
-                });
+                let value = await this.score(token, solcSnapshot)
+                let score = { value: value, time: Date.now() }
+                await this.tokenManager.updateToken(token.address, { score: score })
+                console.log(`[TokenScorer] Token '${token.address}' has been assigned a score of ${score.value}.`)
+                scoring = scoring.filter((item, index, array) => { return item !== token.address })
             }
             console.log(" ")
         }
     }
 
-    async score(token, solc){
-        let contractChecker = new ContractChecker(token, solc)
-        if(token.contract === null) return 0
-        //await contractChecker.compile()
+    async score(token, solc) {
+        if (token.contract === null) return 0
         return 1
     }
 }
